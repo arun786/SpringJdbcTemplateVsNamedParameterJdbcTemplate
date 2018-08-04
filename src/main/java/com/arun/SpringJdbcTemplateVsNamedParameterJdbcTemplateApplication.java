8cd5e31befb7a5c59605616267a61051aef80122
Adwiti789 <adwiti1975@gmail.com>
2018-08-04 12:58:19 -0700

    first commit

---
+++ src/main/java/com/arun/SpringJdbcTemplateVsNamedParameterJdbcTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.arun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcTemplateVsNamedParameterJdbcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcTemplateVsNamedParameterJdbcTemplateApplication.class, args);
+	}
+}
